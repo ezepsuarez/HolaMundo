b6039fefd1d8e04b74c2047a622dfbf110bc06b1
ezepsuarez <ezepsuarez@gmail.com>
2021-03-03 15:45:59 -0300

    Create index.php

---
+++ index.php
@@ -0,0 +1,19 @@
+<table style="border-collapse: collapse; width: 100%; height: 75px;" border="1">
+<tbody>
+<tr>
+<td style="width: 100%;" bgcolor="&quot;#FA4422">Prueba openshift</td>
+</tr>
+</tbody>
+</table>
+<p>&nbsp;</p>
+<table style="border-collapse: collapse; width: 99.4152%; height: 28px;" border="1">
+<tbody>
+<tr>
+<td style="width: 100%;" bgcolor="#AA4412"><span style="color: #ffff00;">Ambiente: 
+<?php
+        include("config.php");
+?></span>
+</td>
+</tr>
+</tbody>
+</table>
